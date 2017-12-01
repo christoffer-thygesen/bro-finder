7169d86043612e9843364e166257065ecafa99cc
Kasper Daugaard <kasdaug@gmail.com>
2017-12-01 11:43:12 +0100

    Register Activity added

--- app/src/main/java/memelord/com/bro_finder/LoginActivity.java
+++ app/src/main/java/memelord/com/bro_finder/LoginActivity.java
@@ -1,8 +1,12 @@
 package memelord.com.bro_finder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,9 +18,17 @@ public class LoginActivity extends AppCompatActivity {
 
     public void passwordRecovery(View v){
 
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width =(int)(dm.widthPixels*.8); //80% Screen size
+        int height =(int)(dm.heightPixels*0.5); //50% Screen size
+
+        new PopupWindow(width,height);
     }
 
     public void userRegister(View v){
-
+Intent intent = new Intent(LoginActivity.class,RegisterUserActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/memelord/com/bro_finder/RegisterUserActivity.java
@@ -0,0 +1,18 @@
+package memelord.com.bro_finder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+
+    public void registerAccount(View V){
+
+    }
+}
