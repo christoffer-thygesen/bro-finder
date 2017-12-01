56bbf0bbfd354e2518ed60a221dd1ea03867c961
Kasper Daugaard <kasdaug@gmail.com>
2017-12-01 13:30:43 +0100

    No errors state at LoginActivity and RegisterAccountActivity

--- app/src/main/java/memelord/com/bro_finder/RegisterUserActivity.java
+++ app/src/main/java/memelord/com/bro_finder/RegisterUserActivity.java
@@ -50,7 +50,7 @@ String password;
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = broAuth.getCurrentUser();
-                            
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
