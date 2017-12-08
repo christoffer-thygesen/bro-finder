a0f28cb9daf6dc8837e4ceec8951987df889d830
Kasper Daugaard <kasdaug@gmail.com>
2017-12-08 11:57:51 +0100

    CheckBoxAll Invoked

--- app/src/main/java/memelord/com/bro_finder/MyEventsActivity.java
+++ app/src/main/java/memelord/com/bro_finder/MyEventsActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,25 +23,56 @@ public class MyEventsActivity extends AppCompatActivity {
 
 
     @Override
-    public  void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_events);
-        final ArrayList<Event> myEventsList = new ArrayList<>();
-        final MyEventsAdapter myAdapter = new MyEventsAdapter(this,myEventsList);
-        final MyEventsAdapter myParticipatingEventsAdapter = new MyEventsAdapter(this,myEventsList);
-
-//        Event a = new Event("123", "Kicking a ball", "Football", "abc", "John", 5, 2,  2017, 06022017, 333, 444);
-//        Event b = new Event("1234", "Kicking a bottle", "Beer", "ab", "Jo", 6, 2,  2017, 06022017, 333, 444);
-//        Event c = new Event("1235", "Kicking ", "Walk", "abcd", "Johnathan", 5, 3,  2017, 06022017, 333, 444);
-//        myEventsList.add(0,a);
-//        myEventsList.add(1,b);
-//        myEventsList.add(2,c);
 
 
-        ListView myEvents = findViewById(R.id.MyEventList);
-        myEvents.setAdapter(myAdapter);
+        final ListView myEvents = findViewById(R.id.MyEventList);
         ListView participatingEvents = findViewById(R.id.myParticipatingEvents);
+        final ArrayList<Event> myEventsList = new ArrayList<>();
+        final MyEventsAdapter myAdapter = new MyEventsAdapter(this, myEventsList);
+        final MyEventsAdapter myParticipatingEventsAdapter = new MyEventsAdapter(this, myEventsList);
+
+
+        Event a = new Event("123", "Kicking a ball", "Football", "abc", "John", 5, 2, 2017, 06022017, 333, 444, 555);
+        Event b = new Event("1234", "Kicking a bottle", "Beer", "ab", "Jo", 6, 2, 2017, 06022017, 333, 444, 555);
+        Event c = new Event("1235", "Kicking ", "Walk", "abcd", "Johnathan", 5, 3, 2017, 06022017, 333, 444, 555);
+        myEventsList.add(0, a);
+        myEventsList.add(1, b);
+        myEventsList.add(2, c);
+
+        CheckBox box = findViewById(R.id.checkboxAll);
+
+        myEvents.setAdapter(myAdapter);
+
         participatingEvents.setAdapter(myParticipatingEventsAdapter);
+
+      box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+          @Override
+
+          public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+              if (isChecked == true){
+              for(int i = 0; i<myEvents.getChildCount();i++){
+                  View mChild = myEvents.getChildAt(i);
+
+                  CheckBox checkitOff = mChild.findViewById(R.id.checkBox);
+                  checkitOff.setChecked(true);
+          }}
+          else if(isChecked != true){
+                  for(int i = 0; i<myEvents.getChildCount();i++){
+                      View mChild = myEvents.getChildAt(i);
+
+                      CheckBox checkitOff = mChild.findViewById(R.id.checkBox);
+                      checkitOff.setChecked(false);
+              }
+
+      }}
+
+    });}
 
 
 
@@ -60,27 +93,18 @@ ListView participatingEvents = (ListView)findViewById(R.id.myParticipatingEvents
 
 
 
-    }
-
-    public void checkBoxAll(View v){
-        /*
-int events = MyEventsActivity().getCount();
-for(int i = 0; i<events;i++){
-myEvents.setItemChecked(i,true);
-}*/}
-
-
     public void refreshButton(View v){
 
         recreate();
         Toast.makeText( MyEventsActivity.this, "Refreshed page",
                 Toast.LENGTH_SHORT).show();
+
     }
 
     public void deleteEvents(View v){
         /*for(int i = 0; i<myEventsList.getCount();i++){
             if(myEventsList.isChecked){
-                //send delete information to database
+               DatabaseManager.getInstance(.deleteEvent();)
             }
         }*/
     }
